0fac22053c6915c0cee1ee3382e6da5604c8b620
RiversideRocks <wilest7@gmail.com>
2021-05-24 23:36:54 +0000

    demo

--- scraper/linkHandler.php
+++ scraper/linkHandler.php
@@ -4,10 +4,24 @@ require "../vendor/autoload.php";
 
 use Embed\Embed;
 
+$q = $_GET["q"];
+
+exec("php DuckDuckGo.php ${q}");
+
 $current_links = json_decode(file_get_contents("links.json"), true);
+
+$titles = [];
+$urls = [];
+
+
 
 foreach($current_links as $l)
 {
   $info = Embed::create("https://" . $l);
-  echo $info->title;
+  //print_r($info->images);
+  foreach($info->images as $imgd)
+  {
+      $img = $imgd["url"];
+      echo "<img src='https://external-hit.riverside.rocks/${img}' />";
+  }
 }
