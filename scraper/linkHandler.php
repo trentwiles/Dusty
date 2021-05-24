f8d7453e3cec96789c9415b5b146431cd54fef94
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2021-05-24 19:20:46 -0400

    Update linkHandler.php

--- scraper/linkHandler.php
+++ scraper/linkHandler.php
@@ -9,5 +9,5 @@ $current_links = json_decode(file_get_contents("links.json"), true);
 foreach($current_links as $l)
 {
   $info = Embed::create($l);
-  echo $info->title;
+  echo "https://" . $info->title;
 }
