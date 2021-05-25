70f4833b14eb2122b31f8c4406827fae9db4b4ab
RiversideRocks <wilest7@gmail.com>
2021-05-25 16:17:51 +0000

    Include homepage in router

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require __DIR__ . "/vendor/autoload.php";
 $router = new \Bramus\Router\Router();
 
 $router->get('/', function() {
-    echo 'OK';
+    require "server/main.php";
 });
 
 $router->run();
