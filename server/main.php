bb26498d3570145d0b7a36f297ec7235b4aaabd2
RiversideRocks <wilest7@gmail.com>
2021-05-25 16:46:14 +0000

    Basic homepage

--- server/main.php
+++ server/main.php
@@ -1,5 +1,17 @@
 <?php
 
 require "../vendor/autoload.php";
-
-echo "Welcome to Dusty!";
+require "head.php";
+?>
+<br><br><br>
+<form method="get">
+  <input type="text" id="q" name="q"><br>
+  <input type="submit" value="Go!">
+</form> 
+<br><br><br>
+<?php
+$conf = json_decode(file_get_contents("../config.json"), true);
+if($conf["custom_html"]){
+  echo $conf["custom_html"];
+}
+?>
